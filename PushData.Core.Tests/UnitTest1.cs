94d9684bf23fc3870c599be84b7c35b74e883eba
paul_blamire <paul_blamire@everhaven.co.uk>
2022-02-25 20:59:55 +0000

    Add sync failing test

--- PushData.Core.Tests/UnitTest1.cs
+++ PushData.Core.Tests/UnitTest1.cs
@@ -1,11 +1,55 @@
+using System.Collections.Generic;
+using System.Diagnostics;
 using Xunit;
 
 namespace PushData.Core.Tests;
 
-public class UnitTest1
+public class DataSyncTests
 {
-    [Fact]
-    public void Test1()
+    public class Item
     {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+    
+    [Fact]
+    public void CanSyncASingleNewItemToOneSink()
+    {
+        var sourceData = new List<Item>()
+        {
+            new Item() { Id = "A", Value = "A" }
+        };
+        var destinationData = new List<Item>();
+        
+        var source = new Source(sourceData);
+        var destination = new Destination(destinationData);
+        var sut = new DataSync();
+        sut.Sync(source, destination);
+
+        Assert.Contains(destinationData, d => d.Id == "A" && d.Value == "A");
+    }
+}
+
+public class DataSync
+{
+    public void Sync(Source source, Destination destination)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class Destination
+{
+    public Destination(List<DataSyncTests.Item> destinationData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class Source
+{
+    public Source(List<DataSyncTests.Item> sourceData)
+    {
+        throw new System.NotImplementedException();
     }
 }
