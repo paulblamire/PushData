a6189d69fd31e41881e122ca2bc9ec2cdb2117f2
paul_blamire <paul_blamire@everhaven.co.uk>
2022-02-25 21:28:05 +0000

    Add ability to filter the source data

--- PushData.Core.Tests/DataSyncTests.cs
+++ PushData.Core.Tests/DataSyncTests.cs
@@ -16,7 +16,7 @@ public class DataSyncTests
         };
         var destinationData = new List<ItemOne>();
 
-        var source = new Source<ItemOne>(sourceData);
+        var source = new ListSource<ItemOne>(sourceData);
         var destination = new Destination<ItemOne>(destinationData);
         var sut = new DataSync();
         sut.Sync(source, destination);
@@ -33,11 +33,32 @@ public class DataSyncTests
         };
         var destinationData = new List<ItemTwo>();
 
-        var source = new Source<ItemTwo>(sourceData);
+        var source = new ListSource<ItemTwo>(sourceData);
         var destination = new Destination<ItemTwo>(destinationData);
         var sut = new DataSync();
         sut.Sync(source, destination);
 
         Assert.Contains(destinationData, d => d.Id == "A" && d.Value == "A");
+    }
+    
+    [Fact]
+    public void CanFilterSourceItems()
+    {
+        var sourceData = new List<ItemOne>()
+        {
+            new ItemOne() { Id = "A", Value = "A" },
+            new ItemOne() { Id = "B", Value = "B" }
+
+        };
+        var destinationData = new List<ItemOne>();
+
+        var innerSource =  new ListSource<ItemOne>(sourceData);
+        var source = new FilterSource<ItemOne>(innerSource, i => i.Id == "A");
+        var destination = new Destination<ItemOne>(destinationData);
+        var sut = new DataSync();
+        sut.Sync(source, destination);
+
+        Assert.Single(destinationData);
+        Assert.Single(destinationData, d => d.Id == "A" && d.Value == "A");
     }
 }
