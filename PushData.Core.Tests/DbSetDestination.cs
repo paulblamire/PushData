933ab32e02b55939702284acb8b1068d6267fcd4
paul_blamire <paul_blamire@everhaven.co.uk>
2022-02-25 22:30:15 +0000

    Add example EF implementations

---
+++ PushData.Core.Tests/DbSetDestination.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace PushData.Core.Tests;
+
+public class DbSetDestination<TItem, TKey> : IDestination<TItem> where TKey : notnull where TItem : class
+{
+    private readonly DbSet<TItem> _destinationData;
+    private readonly DetectChanges<TItem, TKey> _detectChanges;
+    private readonly Func<TItem, TKey> _getKey;
+    private readonly DbContext _dbContext;
+
+    public DbSetDestination(DbContext dbContext, Func<TItem, TKey> getKey)
+    {
+        _dbContext = dbContext;
+        _destinationData = dbContext.Set<TItem>();
+        _getKey = getKey;
+        _detectChanges = new DetectChanges<TItem, TKey>(getKey);
+    }
+
+    public void ApplyChanges(IEnumerable<TItem> sourceData)
+    {
+        _dbContext.Configuration.AutoDetectChangesEnabled = false;
+        
+        _detectChanges.Process(sourceData, _destinationData,
+            items => { _destinationData.AddRange(items); },
+            (k, i) =>
+            {
+                var existingItem = _destinationData.Find(k);
+                _dbContext.Entry(existingItem).CurrentValues.SetValues(i);
+            });
+        
+        _dbContext.Configuration.AutoDetectChangesEnabled = true;
+        _dbContext.SaveChanges();
+    }
+}
---
+++ PushData.Core.Tests/DbSetSource.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Linq;
+
+namespace PushData.Core.Tests;
+
+public class DbSetSource<TItem> : ISource<TItem> where TItem : class
+{
+    private readonly DbContext _dbContext;
+
+    public DbSetSource(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public IQueryable<TItem> GetData()
+    {
+        return _dbContext.Set<TItem>();
+    }
+}
